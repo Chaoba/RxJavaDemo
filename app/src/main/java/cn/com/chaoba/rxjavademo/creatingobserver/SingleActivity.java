e9ce3dcba27104b60202337217a3e69c373e941d
Chaoba <qqliys@126.com>
2017-07-09 08:09:30 +0800

    create singleActivity

---
+++ app/src/main/java/cn/com/chaoba/rxjavademo/creatingobserver/SingleActivity.java
@@ -0,0 +1,68 @@
+package cn.com.chaoba.rxjavademo.creatingobserver;
+
+import android.os.Bundle;
+import android.view.View;
+
+import cn.com.chaoba.rxjavademo.BaseActivity;
+import rx.Single;
+import rx.SingleSubscriber;
+
+public class SingleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLButton.setText("Single data");
+        mLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Single.create(new Single.OnSubscribe<Integer>() {
+                    @Override
+                    public void call(SingleSubscriber<? super Integer> singleSubscriber) {
+                        if (!singleSubscriber.isUnsubscribed()) {
+                            singleSubscriber.onSuccess(1);
+                        }
+                    }
+                }).subscribe(new SingleSubscriber<Integer>() {
+                    @Override
+                    public void onSuccess(Integer value) {
+                        log(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        log(error.getMessage());
+
+                    }
+                });
+            }
+        });
+        mRButton.setText("Single error");
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Single.create(new Single.OnSubscribe<Integer>() {
+                    @Override
+                    public void call(SingleSubscriber<? super Integer> singleSubscriber) {
+                        if (!singleSubscriber.isUnsubscribed()) {
+                            singleSubscriber.onError(new Throwable("Single error"));
+                        }
+                    }
+                }).subscribe(new SingleSubscriber<Integer>() {
+                    @Override
+                    public void onSuccess(Integer value) {
+                        log(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        log(error.getMessage());
+
+                    }
+                });
+            }
+        });
+    }
+
+
+}
