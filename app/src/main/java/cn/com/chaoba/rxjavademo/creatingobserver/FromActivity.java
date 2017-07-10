629699faf6a3184a7a535171f02920b2dbd7bc9d
Li Yanshun <liyanshun@megvii.com>
2017-07-10 09:14:35 +0800

    modify from

--- app/src/main/java/cn/com/chaoba/rxjavademo/creatingobserver/FromActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/creatingobserver/FromActivity.java
@@ -1,12 +1,14 @@
 package cn.com.chaoba.rxjavademo.creatingobserver;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
+import rx.functions.Action1;
 
 public class FromActivity extends BaseActivity {
     Integer[] arrays = {0, 1, 2, 3, 4, 5};
@@ -20,8 +22,28 @@ public class FromActivity extends BaseActivity {
         }
         mLButton.setText("FromArray");
         mRButton.setText("FromIterable");
-        mLButton.setOnClickListener(e -> FromArray().subscribe(i -> log("FromArray:" + i)));
-        mRButton.setOnClickListener(e -> FromIterable().subscribe(i -> log("FromIterable:" + i)));
+        mLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FromArray().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer i) {
+                        log("FromArray:" + i);
+                    }
+                });
+            }
+        });
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FromIterable().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer i) {
+                        log("FromIterable:" + i);
+                    }
+                });
+            }
+        });
     }
 
     private Observable<Integer> FromArray() {
