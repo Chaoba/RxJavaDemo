6c3be06e6e62dd74c23e35ef840b7a74958af227
chaoba <qqliys@126.com>
2017-07-11 21:35:29 +0800

    modify repeat and timer

--- app/src/main/java/cn/com/chaoba/rxjavademo/creatingobserver/RepeatAndTimerActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/creatingobserver/RepeatAndTimerActivity.java
@@ -1,12 +1,14 @@
 package cn.com.chaoba.rxjavademo.creatingobserver;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.concurrent.TimeUnit;
 
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 
 public class RepeatAndTimerActivity extends BaseActivity {
@@ -15,13 +17,33 @@ public class RepeatAndTimerActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("repeat");
-        mLButton.setOnClickListener(e -> repeatObserver().subscribe(i -> log("repeat:" + i)));
+        mLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                repeatObserver().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer i) {
+                        log("repeat:" + i);
+                    }
+                });
+            }
+        });
         mRButton.setText("timer");
-        mRButton.setOnClickListener(e -> timerObserver().subscribe(i -> log("timer:" + i)));
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timerObserver().subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        log("timer:" + aLong);
+                    }
+                });
+            }
+        });
     }
 
     private Observable<Integer> repeatObserver() {
-        return Observable.just(1).repeat(5);
+        return Observable.just(1,2,3).repeat(3);
     }
 
     private Observable<Long> timerObserver() {
