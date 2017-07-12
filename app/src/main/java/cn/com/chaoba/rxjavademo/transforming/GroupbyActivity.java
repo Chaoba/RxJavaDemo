015718b6707056d031de3c094c2d7e5ffe12ff3b
chaoba <qqliys@126.com>
2017-07-12 20:51:28 +0800

    modify groupby

--- app/src/main/java/cn/com/chaoba/rxjavademo/transforming/GroupbyActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/transforming/GroupbyActivity.java
@@ -1,10 +1,12 @@
 package cn.com.chaoba.rxjavademo.transforming;
 
 import android.os.Bundle;
+import android.view.View;
 
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
-import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.observables.GroupedObservable;
 
 public class GroupbyActivity extends BaseActivity {
@@ -13,49 +15,66 @@ public class GroupbyActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("groupBy");
-        mLButton.setOnClickListener(e -> groupByObserver().subscribe(new Subscriber<GroupedObservable<Integer, Integer>>() {
+        mLButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompleted() {
-
+            public void onClick(View v) {
+                groupByObserver().subscribe(new Action1<GroupedObservable<Integer, Integer>>() {
+                    @Override
+                    public void call(GroupedObservable<Integer, Integer> groupedObservable) {
+                        groupedObservable.count().subscribe(new Action1<Integer>() {
+                            @Override
+                            public void call(Integer integer) {
+                                log("key" + groupedObservable.getKey()
+                                        + " contains:" + integer + " numbers");
+                            }
+                        });
+                    }
+                });
             }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(GroupedObservable<Integer, Integer> groupedObservable) {
-                groupedObservable.count().subscribe(integer -> log("key" + groupedObservable.getKey() + " contains:" + integer + " numbers"));
-            }
-        }));
+        });
         mRButton.setText("groupByKeyValue");
-        mRButton.setOnClickListener(e -> groupByKeyValueObserver().subscribe(new Subscriber<GroupedObservable<Integer, String>>() {
+        mRButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompleted() {
-
+            public void onClick(View v) {
+                groupByStringObserver().subscribe(new Action1<GroupedObservable<Integer, String>>() {
+                    @Override
+                    public void call(GroupedObservable<Integer, String> groupedObservable) {
+                        if (groupedObservable.getKey() == 0) {
+                            groupedObservable.subscribe(new Action1<String>() {
+                                @Override
+                                public void call(String s) {
+                                    log(s);
+                                }
+                            });
+                        }
+                    }
+                });
             }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(GroupedObservable<Integer, String> integerIntegerGroupedObservable) {
-                if (integerIntegerGroupedObservable.getKey() == 0) {
-                    integerIntegerGroupedObservable.subscribe(integer -> log(integer));
-                }
-            }
-        }));
+        });
     }
 
     private Observable<GroupedObservable<Integer, Integer>> groupByObserver() {
-        return Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9).groupBy(integer -> integer % 2);
+        return Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .groupBy(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return integer % 2;
+                    }
+                });
     }
 
-    private Observable<GroupedObservable<Integer, String>> groupByKeyValueObserver() {
+    private Observable<GroupedObservable<Integer, String>> groupByStringObserver() {
         return Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
-                .groupBy(integer -> integer % 2, integer -> "groupByKeyValue:" + integer);
+                .groupBy(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return integer % 2;
+                    }
+                }, new Func1<Integer, String>() {
+                    @Override
+                    public String call(Integer integer) {
+                        return "groupByKeyValue:" + integer;
+                    }
+                });
     }
 }
