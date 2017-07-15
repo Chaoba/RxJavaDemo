ed4284c94998ec91cb89bec69cecee7ae5873397
Li Yanshun <liyanshun@megvii.com>
2017-07-15 21:58:29 +0800

    modify skip and take

--- app/src/main/java/cn/com/chaoba/rxjavademo/Filtering/SkipAndTakeActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/Filtering/SkipAndTakeActivity.java
@@ -1,9 +1,11 @@
 package cn.com.chaoba.rxjavademo.filtering;
 
 import android.os.Bundle;
+import android.view.View;
 
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
+import rx.functions.Action1;
 
 public class SkipAndTakeActivity extends BaseActivity {
 
@@ -11,14 +13,35 @@ public class SkipAndTakeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("Skip");
-        mLButton.setOnClickListener(e -> skipObserver().subscribe(i -> log("Skip:" + i)));
+        mLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipObserver().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer i) {
+                        log("Skip:" + i);
+                    }
+                });
+            }
+        });
         mRButton.setText("Take");
-        mRButton.setOnClickListener(e -> takeObserver().subscribe(i -> log("Take:" + i)));
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeObserver().subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer i) {
+                        log("Take:" + i);
+                    }
+                });
+            }
+        });
     }
 
     private Observable<Integer> skipObserver() {
         return Observable.just(0, 1, 2, 3, 4, 5).skip(2);
     }
+
     private Observable<Integer> takeObserver() {
         return Observable.just(0, 1, 2, 3, 4, 5).take(2);
     }
