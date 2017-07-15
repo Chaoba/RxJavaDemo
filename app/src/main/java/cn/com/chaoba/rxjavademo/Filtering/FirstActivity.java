b2d7e66b5f5ab7da14220a4dc325ad2b59edafa0
Li Yanshun <liyanshun@megvii.com>
2017-07-15 21:51:08 +0800

    modify first

--- app/src/main/java/cn/com/chaoba/rxjavademo/Filtering/FirstActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/Filtering/FirstActivity.java
@@ -1,11 +1,12 @@
 package cn.com.chaoba.rxjavademo.filtering;
 
 import android.os.Bundle;
+import android.view.View;
 
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
-import rx.Subscriber;
-import rx.observables.BlockingObservable;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class FirstActivity extends BaseActivity {
 
@@ -13,36 +14,40 @@ public class FirstActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("First");
-        mLButton.setOnClickListener(e -> firstObserver().subscribe(i -> log("First:" + i)));
-        mRButton.setText(" Blocking");
-        mRButton.setOnClickListener(e -> {
-            log("blocking:" + filterObserver().first(i -> i > 1));
+        mLButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Observable.just(0, 1, 2, 3, 4, 5).first(new Func1<Integer, Boolean>() {
+                            @Override
+                            public Boolean call(Integer integer) {
+                                return integer > 3;
+                            }
+                        }).subscribe(new Action1<Integer>() {
+                            @Override
+                            public void call(Integer i) {
+                                log("First:" + i);
+                            }
+                        });
+                    }
+                });
+
+
+        mRButton.setText("Blocking");
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = Observable.just(0, 1, 2, 3, 4, 5)
+                        .toBlocking()
+                        .first(new Func1<Integer, Boolean>() {
+                            @Override
+                            public Boolean call(Integer i) {
+                                return i > 1;
+                            }
+                        });
+                log("blocking:" + result);
+            }
         });
     }
 
-    private Observable<Integer> firstObserver() {
-        return Observable.just(0, 1, 2, 3, 4, 5).first(i -> i > 1);
-    }
-
-    private BlockingObservable<Integer> filterObserver() {
-        return Observable.create(new Observable.OnSubscribe<Integer>() {
-            @Override
-            public void call(Subscriber<? super Integer> subscriber) {
-                for (int i = 0; i < 5; i++) {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    if (!subscriber.isUnsubscribed()) {
-                        log("onNext:" + i);
-                        subscriber.onNext(i);
-                    }
-                }
-                if (!subscriber.isUnsubscribed()) {
-                    subscriber.onCompleted();
-                }
-            }
-        }).toBlocking();
-    }
 }
