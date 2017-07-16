0af58282721e9df741c4f6e17a1a2d184292669a
Chaoba <qqliys@126.com>
2017-07-16 21:11:49 +0800

    modify zip

--- app/src/main/java/cn/com/chaoba/rxjavademo/combining/ZipActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/combining/ZipActivity.java
@@ -2,10 +2,14 @@ package cn.com.chaoba.rxjavademo.combining;
 
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
-import rx.Subscriber;
-import rx.schedulers.Schedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.functions.Func3;
 
 public class ZipActivity extends BaseActivity {
 
@@ -13,33 +17,53 @@ public class ZipActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("zipWith");
-        mLButton.setOnClickListener(e -> zipWithObserver().subscribe(i -> log("zipWith:" + i + "\n")));
+        mLButton.setOnClickListener(e -> {
+            zipWithObserver().subscribe(new Action1<String>() {
+                @Override
+                public void call(String s) {
+                    log("zipWith:" + s);
+                }
+            });
+        });
         mRButton.setText("zip");
-        mRButton.setOnClickListener(e -> zipWithIterableObserver().subscribe(i -> log("zip:" + i + "\n")));
+        mRButton.setOnClickListener(e -> {
+            zipWithIterableObserver().subscribe(new Action1<String>() {
+                @Override
+                public void call(String s) {
+                    log("zip:" + s);
+                }
+            });
+        });
     }
 
     private Observable<String> zipWithObserver() {
-        return createObserver(2).zipWith(createObserver(3), (s, s2) -> s + "-" + s2);
+        return createObserver(2).zipWith(createObserver(3),
+                new Func2<String, String, String>() {
+                    @Override
+                    public String call(String s, String s2) {
+                        return s + "-" + s2;
+                    }
+                });
     }
 
     private Observable<String> zipWithIterableObserver() {
-        return Observable.zip(createObserver(2), createObserver(3), createObserver(4), (s, s2, s3) -> s + "-" + s2 + "-" + s3);
+        return Observable.zip(createObserver(2),
+                createObserver(3), createObserver(4),
+                new Func3<String, String, String, String>() {
+                    @Override
+                    public String call(String s, String s2, String s3) {
+                        return s + "-" + s2 + "-" + s3;
+                    }
+                });
     }
 
     private Observable<String> createObserver(int index) {
-        return Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                for (int i = 1; i <= index; i++) {
-                    log("emitted:" + index + "-" + i);
-                    subscriber.onNext(index + "-" + i);
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        return Observable.interval(100, TimeUnit.MILLISECONDS).take(index)
+                .map(new Func1<Long, String>() {
+                    @Override
+                    public String call(Long aLong) {
+                        return index + ":" + aLong;
                     }
-                }
-            }
-        }).subscribeOn(Schedulers.newThread());
+                });
     }
 }
