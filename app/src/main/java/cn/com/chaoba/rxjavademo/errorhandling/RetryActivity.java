522cb0f2c1d6c2e64b8bbaab658a178a5373796b
Chaoba <qqliys@126.com>
2017-07-17 11:09:00 +0800

    modify retry

--- app/src/main/java/cn/com/chaoba/rxjavademo/errorhandling/RetryActivity.java
+++ app/src/main/java/cn/com/chaoba/rxjavademo/errorhandling/RetryActivity.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 import cn.com.chaoba.rxjavademo.BaseActivity;
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Func1;
+import rx.functions.Func2;
 
 public class RetryActivity extends BaseActivity {
 
@@ -14,39 +16,43 @@ public class RetryActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLButton.setText("retry");
-        mLButton.setOnClickListener(e -> retryObserver().subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onCompleted() {
-                log("retry-onCompleted\n");
-            }
+        mLButton.setOnClickListener(e -> {
+            retryObserver().subscribe(new Subscriber<Integer>() {
+                @Override
+                public void onCompleted() {
+                    log("retry-onCompleted");
+                }
 
-            @Override
-            public void onError(Throwable e) {
-                log("retry-onError:" + e.getMessage()+"\n");
-            }
+                @Override
+                public void onError(Throwable e) {
+                    log("retry-onError:" + e.getMessage());
+                }
 
-            @Override
-            public void onNext(Integer s) {
-                log("retry-onNext:" + s);
-            }
-        }));
+                @Override
+                public void onNext(Integer s) {
+                    log("retry-onNext:" + s);
+                }
+            });
+        });
         mRButton.setText("retryWhen");
-        mRButton.setOnClickListener(e -> retryWhenObserver().subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onCompleted() {
-                log("retryWhen-onCompleted\n");
-            }
+        mRButton.setOnClickListener(e -> {
+            retryWhenObserver().subscribe(new Subscriber<Integer>() {
+                @Override
+                public void onCompleted() {
+                    log("retryWhen-onCompleted");
+                }
 
-            @Override
-            public void onError(Throwable e) {
-                log("retryWhen-onError:" + e.getMessage()+"\n");
-            }
+                @Override
+                public void onError(Throwable e) {
+                    log("retryWhen-onError:" + e.getMessage());
+                }
 
-            @Override
-            public void onNext(Integer s) {
-                log("retryWhen-onNext:" + s);
-            }
-        }));
+                @Override
+                public void onNext(Integer s) {
+                    log("retryWhen-onNext:" + s);
+                }
+            });
+        });
     }
 
     private Observable<Integer> retryObserver() {
@@ -54,24 +60,36 @@ public class RetryActivity extends BaseActivity {
     }
 
     private Observable<Integer> retryWhenObserver() {
-        return createObserver().retryWhen(observable -> observable.zipWith(Observable.just(1, 2, 3),
-                (throwable, integer) -> throwable.getMessage() + integer)
-                .flatMap(throwable -> {
-                    log(throwable);
-                    return Observable.timer(1, TimeUnit.SECONDS);
-                }));
-
+        return createObserver().retryWhen(
+                new Func1<Observable<? extends Throwable>, Observable<?>>() {
+                    @Override
+                    public Observable<?> call(Observable<? extends Throwable> observable) {
+                        return observable.zipWith(Observable.just(1, 2, 3),
+                                new Func2<Throwable, Integer, String>() {
+                                    @Override
+                                    public String call(Throwable throwable, Integer integer) {
+                                        return throwable.getMessage() + integer;
+                                    }
+                                })
+                                .flatMap(new Func1<String, Observable<Long>>() {
+                                    @Override
+                                    public Observable<Long> call(String s) {
+                                        log(s);
+                                        return Observable.timer(1, TimeUnit.SECONDS);
+                                    }
+                                });
+                    }
+                });
     }
 
     private Observable<Integer> createObserver() {
         return Observable.create(new Observable.OnSubscribe<Integer>() {
-
             @Override
             public void call(Subscriber<? super Integer> subscriber) {
                 log("subscribe");
                 for (int i = 0; i < 3; i++) {
                     if (i == 2) {
-                        subscriber.onError(new Exception("Exception-"));
+                        subscriber.onError(new Exception("Exception"));
                     } else {
                         subscriber.onNext(i);
                     }
