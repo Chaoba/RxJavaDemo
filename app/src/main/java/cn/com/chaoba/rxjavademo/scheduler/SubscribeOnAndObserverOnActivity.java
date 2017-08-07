ee229707224a541de4d7f06193adbaa0c60c5e35
Li Yanshun <liyanshun@megvii.com>
2017-08-07 10:59:03 +0800

    modify subscribeon and Observeron

---
+++ app/src/main/java/cn/com/chaoba/rxjavademo/scheduler/SubscribeOnAndObserverOnActivity.java
@@ -0,0 +1,142 @@
+package cn.com.chaoba.rxjavademo.scheduler;
+
+import android.os.Bundle;
+import android.view.View;
+
+import cn.com.chaoba.rxjavademo.BaseActivity;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+public class SubscribeOnAndObserverOnActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                log("start:" + Thread.currentThread()
+                        .getName());
+                subscriber.onNext(1);
+                subscriber.onCompleted();
+            }
+        })
+                .subscribeOn(Schedulers.newThread())
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        log(integer + ":" + Thread.currentThread()
+                                .getName());
+                        return integer + 1;
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        log(integer + ":" + Thread.currentThread()
+                                .getName());
+                        return integer + 1;
+                    }
+                })
+                .subscribeOn(Schedulers.computation())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        log("action:" + Thread.currentThread()
+                                .getName());
+                    }
+                });
+
+        mLButton.setText("observeOn");
+        mLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Observable.create(new Observable.OnSubscribe<Integer>() {
+                    @Override
+                    public void call(Subscriber<? super Integer> subscriber) {
+                        log("start:" + Thread.currentThread()
+                                .getName());
+                        subscriber.onNext(1);
+                        subscriber.onCompleted();
+                    }
+                })
+                        .observeOn(Schedulers.newThread())
+                        .map(new Func1<Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer integer) {
+                                log(integer + ":" + Thread.currentThread()
+                                        .getName());
+                                return integer + 1;
+                            }
+                        })
+                        .observeOn(Schedulers.io())
+                        .map(new Func1<Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer integer) {
+                                log(integer + ":" + Thread.currentThread()
+                                        .getName());
+                                return integer + 1;
+                            }
+                        })
+                        .observeOn(Schedulers.computation())
+                        .subscribe(new Action1<Integer>() {
+                            @Override
+                            public void call(Integer integer) {
+                                log("action:" + Thread.currentThread()
+                                        .getName());
+                            }
+                        });
+            }
+        });
+
+        mRButton.setText("subscribeOn");
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Observable.create(new Observable.OnSubscribe<Integer>() {
+                    @Override
+                    public void call(Subscriber<? super Integer> subscriber) {
+                        log("start:" + Thread.currentThread()
+                                .getName());
+                        subscriber.onNext(1);
+                        subscriber.onCompleted();
+                    }
+                })
+                        .subscribeOn(Schedulers.newThread())
+                        .map(new Func1<Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer integer) {
+                                log(integer + ":" + Thread.currentThread()
+                                        .getName());
+                                return integer + 1;
+                            }
+                        })
+                        .subscribeOn(Schedulers.io())
+                        .map(new Func1<Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer integer) {
+                                log(integer + ":" + Thread.currentThread()
+                                        .getName());
+                                return integer + 1;
+                            }
+                        })
+                        .subscribeOn(Schedulers.computation())
+                        .subscribe(new Action1<Integer>() {
+                            @Override
+                            public void call(Integer integer) {
+                                log("action:" + Thread.currentThread()
+                                        .getName());
+                            }
+                        });
+            }
+        });
+
+
+    }
+
+}
